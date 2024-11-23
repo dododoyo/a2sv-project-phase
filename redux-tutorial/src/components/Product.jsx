879d2a0976ebc82781142d4a64fe4026884c8eba
dododoyo <dolphinmulugeta88@gmail.com>
2024-11-23 13:19:36 +0300

    Update

--- redux-tutorial/src/components/Product.jsx
+++ redux-tutorial/src/components/Product.jsx
@@ -1,13 +1,13 @@
-import { useGetProductByIdQuery } from "../app/service/products"
+import { useGetProductByIdQuery } from "../app/service/products";
 const Product = () => {
-  const {data,isError,isLoading} = useGetProductByIdQuery(2);
+  const { data, isError, isLoading } = useGetProductByIdQuery(2);
   if (isError) {
     return <h1>Something Went Wrong, Try Again !</h1>;
   }
   if (isLoading) {
     return <h1>Loading . . . </h1>;
   }
-console.log(data)
+  // console.log(data);
   return (
     <>
       <h1>{data.title}</h1>
@@ -19,6 +19,6 @@ console.log(data)
       ))} */}
     </>
   );
-}
+};
 
-export default Product
+export default Product;
